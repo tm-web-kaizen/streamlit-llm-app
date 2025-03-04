a786e8a555a057068b64bc7dd9e2c254cefb7569
tm <toshiya.murayama.web.kaizen@gmail.com>
2025-03-04 22:42:09 +0900

    app.py変更

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from dotenv import load_dotenv
 import streamlit as st
-load_dotenv()
+import os
+from dotenv import load_dotenv
+
+# Streamlit Cloud では `st.secrets` を使い、ローカルでは `.env` を使用
+#try:
+os.environ["OPENAI_API_KEY"]  = st.secrets["OPENAI_API_KEY"]  
+# Streamlit Cloud
+#except FileNotFoundError:
+#    load_dotenv()
+#    api_key = os.getenv("OPENAI_API_KEY", "")
 
 st.title("専門家に相談できます")
 st.write("#### 相談する専門ジャンルを選んでください")
