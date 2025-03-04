2f1bb6b88b35bdcd2591e138002f0e45c2dd5ad8
tm <toshiya.murayama.web.kaizen@gmail.com>
2025-03-04 22:03:23 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import streamlit as st
+load_dotenv()
+
+st.title("専門家に相談できます")
+st.write("#### 相談する専門ジャンルを選んでください")
+st.write("ラジオボタンで専門ジャンルを選んでください")
+st.write("#### 相談内容を入力フォームに入力してください")
+
