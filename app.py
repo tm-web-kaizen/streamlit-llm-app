bb911bc8901d94d1a0156222cb402d4652ec5621
tm <toshiya.murayama.web.kaizen@gmail.com>
2025-03-04 23:10:43 +0900

    app.pyを修正

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import os
 #from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain.schema import SystemMessage, HumanMessage
 
 # Streamlit Cloud では `st.secrets` を使い、ローカルでは `.env` を使用
 #try:
@@ -14,4 +16,24 @@ st.title("専門家に相談できます")
 st.write("#### 相談する専門ジャンルを選んでください")
 st.write("ラジオボタンで専門ジャンルを選んでください")
 st.write("#### 相談内容を入力フォームに入力してください")
+
+
+selected_genre = st.radio("どの専門家に相談したいかを選択してください", ["和食の専門家","中華の専門家","洋食の専門家"])
+input_message = st.text_input(label="相談内容を入力してください。")
+
+
+if st.button("実行する"):
+    st.divider()
+
+    llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0.5)
+
+    message = [
+        SystemMessage(content=f"あなたは、料理のプロフェッショナルです。特に、{selected_genre}です。的確なアドバイスができる料理専門家です"),
+        HumanMessage(content=input_message)
+    ]
+
+    result = llm(message)
+
+
+    st.write(f"回答：{result.content}")
 
